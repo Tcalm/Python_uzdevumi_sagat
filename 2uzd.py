546a8fcab2c1ca01d398795f24d54772450d4f2e
Tcalm <justnormal1324@gmail.com>
2023-11-03 14:38:11 +0200

    izdarīts 2.uzd

--- 2uzd.py
+++ 2uzd.py
@@ -2,3 +2,18 @@
 Iegūt ziņas, izmantojot RSS plūsmu no google.lv.
 
 """
+import feedparser
+
+# URL uz RSS plūsmu
+
+rss_url="https://news.google.com/home?hl=en-LV&gl=LV&ceid=LV:en"
+
+# iegūst Rss plūsmas datus
+kkk=feedparser.parse(rss_url)
+
+# parāda un noformē
+
+for entry in kkk.entries:
+    print("Virsraksts: ", entry.title)
+    print("Saite: ", entry.link)
+    print("\n")
